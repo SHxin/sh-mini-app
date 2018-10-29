eb3d81397cf5f62cc8758688465e4eeb4abb67d3
shenxin <xin.shen@idiaoyan.com>
2018-10-29 17:31:22 +0800

    button 拿用户信息和formid 原生下拉刷新页面demo

---
+++ pages/list02/list02.js
@@ -0,0 +1,15 @@
+// pages/list02/list02.js
+Page({
+  data: {
+
+  },
+  onLoad: function(options) {
+
+  },
+  getFormID: function(e) {
+    console.log(e.detail.formId)
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+  }
+})
